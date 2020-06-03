1ea6f94ee86d19368193c9970ef8f55d440aefaa
mnlmaier <mail@manuelmaier.com>
2020-06-03 19:44:10 +0200

    feat(map): prevent map controls

---
+++ modules/map/Map.vue
@@ -0,0 +1,49 @@
+<template>
+  <GMap
+    ref="gMap"
+    :zoom="zoom"
+    :center="{ lat: lat, lng: long }"
+    :options="mapOptions"
+  ></GMap>
+</template>
+
+<script>
+import { ClassNames as GlobalClassNames } from '~/shared/constants'
+
+export default {
+  props: {
+    items: {
+      type: Array
+    },
+    xclass: {
+      type: String
+    }
+  },
+  data() {
+    return {
+      rootClassName: `${GlobalClassNames.PREFIX}-mMap`,
+      lat: 39.8097343,
+      long: -98.5556199,
+      zoom: 5,
+      mapOptions: {
+        zoomControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        streetViewControl: false,
+        rotateControl: false,
+        fullscreenControl: false,
+        gestureHandling: 'none'
+      }
+    }
+  },
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss">
+.GMap__Wrapper {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,16 @@
 <template>
-  <GMap ref="gMap" :zoom="zoom" :center="{ lat: lat, lng: long }">
-    <GMapMarker :position="{ lat: lat, lng: long }"></GMapMarker>
-  </GMap>
+  <Map />
 </template>
 
 <script>
+import Map from '~/modules/map/Map.vue'
+
 export default {
-  data() {
-    return {
-      lat: 0,
-      long: 0,
-      zoom: 2
-    }
+  components: {
+    Map
   },
-  mounted() {
-    this.$geolocation.getCurrentPosition().then((position) => {
-      console.log(position)
-      this.lat = position.coords.latitude
-      this.long = position.coords.longitude
-      this.zoom = 16
-    })
-  }
+  data() {}
 }
 </script>
 
-<style lang="scss">
-.GMap__Wrapper {
-  width: 100vw;
-  height: 100vh;
-}
-</style>
+<style lang="scss"></style>
